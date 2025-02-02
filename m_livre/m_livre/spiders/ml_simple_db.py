11d6fdd05202b38119d623a2e8cd0d25051d2851
arthbraveheart <arthur.braveheart@estudante.ufjf.br>
2025-02-02 14:57:06 -0300

    collect right prices for mercado livre

--- m_livre/m_livre/spiders/ml_simple_db.py
+++ m_livre/m_livre/spiders/ml_simple_db.py
@@ -5,40 +5,51 @@ import csv
 import time
 from .tools import load_pkl
 from .settings import out_path
+from m_livre.items import ProductItem
 
-class MercadoLivreSpider(scrapy.Spider):
-    name = 'mercado_livre'
+class MagaluSpider(scrapy.Spider):
+    name = 'ml_simple_db'
     today = time.strftime("%d-%m-%Y")
-    search = load_pkl('dular_eans').iloc[:10,:]
-    base_url = 'https://lista.mercadolivre.com.br/{}#D[A:{}]'
+    search = load_pkl('dular_eans')#.iloc[:20,:]
+    base_url = 'https://www.magazineluiza.com.br/busca/{}/?from=submit'
 
     def start_requests(self):
         for i, row in self.search.iterrows():
             ean = row['Ean']
-            url = self.base_url.format(ean, ean)
+            url = self.base_url.format(ean)
             yield scrapy.Request(
                 url,
+                headers={
+                    "x-oxylabs-geo-location":"Brazil",
+                },
                 meta={
-
-                    "row": row,
+                    "proxy": 'https://bravebrave_xJSab:Proxy_1728_Brave@unblock.oxylabs.io:60000',
+                    "ean": ean,
+                    "dont_verify_ssl": True,
                 },
                 callback=self.parse
             )
 
     async def parse(self, response):
-        row = response.meta["row"]
+        ean = response.meta["ean"]
         element = response.xpath('//script[@type="application/ld+json"]/text()').get()
         names, prices, urls = self.get_things_done(element)
         for name, price, url in zip(names, prices, urls):
             if name != 'empty':
-                linha = [name, price.replace('.', ','), url] + row.to_list()
-                self.save_to_csv(linha)
-            else:
-                continue
+                linha = dict(name=name, price=price.replace('.', ','), url=url, ean=ean, ) #+ row.to_list()
+                #products_items = ProductItem(**linha)
+                products_items = ProductItem()
+                products_items['name'] = name
+                products_items['price'] = price#.replace('.', ',')
+                products_items['url'] = url
+                products_items['ean'] = ean
+                yield products_items
+                #self.save_to_csv(linha)
+
 
     def get_things_done(self, element):
         try:
-            pattern_price = re.compile(r'"price":(\d+\.\d{2}|\d+)')
+            pattern_price = re.compile(r'"price":"(\d+\.\d{2}|\d+)"')
             pattern_name = re.compile(r'"Product","name":"(.*?)"')
             pattern_url = re.compile(r'"url":"(.*?)"')
             prices = re.findall(pattern_price, element)
@@ -47,9 +58,9 @@ class MercadoLivreSpider(scrapy.Spider):
             return names, prices, url
         except:
             print("****************** Fail to get PRICE *******************")
-            return ['empty'], ['empty'], ['empty']
+            return ['empty'] , ['empty'] , ['empty']
 
     def save_to_csv(self, linha):
-        with open(f"{out_path}/Prices_M_Livre_{self.today}.csv", "a", newline="", encoding="utf-8-sig") as f:
+        with open(f"{out_path}/Prices_Magalu_{self.today}.csv", "a", newline="", encoding="utf-8-sig") as f:
             csv_writer = csv.writer(f, delimiter=';')
             csv_writer.writerow(linha)
