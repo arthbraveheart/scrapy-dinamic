d4d433b93aa39e15db9963747470893ea376f692
brave <arthur.braveheart@estudante.ufjf.br>
2025-04-23 15:36:23 -0300

    fix layout dash

--- core/apps/dash_apps/raw/raw.py
+++ core/apps/dash_apps/raw/raw.py
@@ -27,7 +27,6 @@ ag_table_1 = html.Div(id='table', className="row")
 
 map_container = html.Div([
     html.Div([
-        html.Button("Download CSV", id="csv-button", n_clicks=0, className="btn btn-primary mb-0 toast-btn g-col-2 p-2"),
         dcc.DatePickerRange(
             id='my-date-picker-range',
             min_date_allowed=date(2023, 12, 1),
@@ -36,9 +35,10 @@ map_container = html.Div([
             start_date=date.today(),
             end_date=(date.today()).replace(day=(date.today()).day + 1),
             display_format='DD MMMM YYYY',
-            className="g-col-2"
+            className="p-2 g-col-1"
         ),
-    ], className="p-2 row grid"),
+        html.Button("Download CSV", id="csv-button", n_clicks=0, className="btn btn-primary mb-0 toast-btn p-2 g-col-1"),
+    ], className="p-2 grid"),
     dcc.Loading(
         id="loading-table",
         type="dot",  # You can choose from "default", "circle", "dot", or "graph"
